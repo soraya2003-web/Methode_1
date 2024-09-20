82a1404fff2497b4a27fcd34d7d7840cbdc30384
gayaaris <rachidaifri90@gmail.com>
2024-09-19 21:54:11 -0400

    alphabet

---
+++ src/rachida/Alphabet.java
@@ -0,0 +1,11 @@
+package rachida;
+
+public class Alphabet {
+    // print alphabet
+    public static void main(String[] args) {
+
+        for (char letter = 'a'; letter <= 'z'; letter++) {
+            System.out.print(letter +" ");
+        }
+    }
+}
